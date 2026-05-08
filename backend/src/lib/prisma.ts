49e6a6bc9ef6b31be7e67591544cc99083485a2f
Hrithik Mishra <littlehrithik9594@gmail.com>
2026-05-08 11:32:16 +0530

    prisma modified

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -2,14 +2,13 @@ import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
 
-// Just pass the configuration object directly!
 const adapter = new PrismaMariaDb({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    port: Number(process.env.PORT),
-    ssl: { 
+    port: Number(process.env.DATABASE_PORT),
+    ssl: {
         rejectUnauthorized: false
     }
 });
